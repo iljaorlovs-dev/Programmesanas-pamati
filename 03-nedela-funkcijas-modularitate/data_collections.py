ecc9704bb070aa99cf0b1887baf2e06040342f87
Ilja Orlovs <ilja.orlovs@gmail.com>
2026-03-22 21:35:30 +0200

    test

--- 03-nedela-funkcijas-modularitate/data_collections.py
+++ 03-nedela-funkcijas-modularitate/data_collections.py
@@ -1,1 +1,10 @@
 #test test 
+empty_dict = {}
+person = {
+'first_name': 'Asabeneh', # str
+'age': 250, # int
+'is_married': True, # bool
+'skills': ['JS','React'], # list
+'address': {'street':'Space', # nested dict
+'zip':'02210'}
+}
