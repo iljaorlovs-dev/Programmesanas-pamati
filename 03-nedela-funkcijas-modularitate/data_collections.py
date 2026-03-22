fea32e37e17b90ce14f928d64007cdb2da576e60
Ilja Orlovs <ilja.orlovs@gmail.com>
2026-03-22 22:27:54 +0200

    totals and numbers

--- 03-nedela-funkcijas-modularitate/data_collections.py
+++ 03-nedela-funkcijas-modularitate/data_collections.py
@@ -1,10 +1,27 @@
-#test test 
-empty_dict = {}
-person = {
-'first_name': 'Asabeneh', # str
-'age': 250, # int
-'is_married': True, # bool
-'skills': ['JS','React'], # list
-'address': {'street':'Space', # nested dict
-'zip':'02210'}
-}
+# --- A daļa — Saraksti ---
+print("--- Lists ---")
+
+numbers = [2, 4, 6, 8, 10]
+
+# add element
+numbers.append(12)
+print(f"After append: {numbers}")
+
+# remove element
+# Удаляем последний элемент
+removed_number = numbers.pop()
+print(f"After pop: {numbers}")
+print(f"Removed number: {removed_number}")
+
+
+#average and count
+total = 0
+count = 0
+
+for number in numbers:
+    total += number
+    count += 1
+average = total / count
+
+
+
